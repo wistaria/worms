b56a3ba4cb334d70ec9fdfa44be6d3b11561d4a5
Synge Todo <synge.todo@gmail.com>
2013-09-12 07:41:56 +0900

    Added version file

---
+++ worms/version.hpp
@@ -0,0 +1,17 @@
+/*****************************************************************************
+*
+* worms: a simple worm code
+*
+* Copyright (C) 2013 by Synge Todo <wistaria@comp-phys.org>
+*
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*****************************************************************************/
+
+#ifndef WORMS_VERSION_HPP
+#define WORMS_VERSION_HPP
+
+#define WORMS_VERSION ""
+
+#endif // WORMS_VERSION_HPP
